fc2e0d0961210c313ee71413e66dd4cbaebb8fa7
Taylor, Ash <ashley.taylor@sungard.com>
2015-12-17 17:55:37 +0000

    average price thing

--- levels/chock_a_block.js
+++ levels/chock_a_block.js
@@ -1,10 +1,11 @@
 process.chdir(__dirname);
 var agent = require('superagent'),
-	config = require('../config');
+	config = require('../config'),
+	$q = require('q');
 
-config.venue = 'FRMEX';
-config.ticker = 'IYQU';
-config.account = 'SAR80403386';
+config.venue = 'GYTDEX';
+config.ticker = 'TOLM';
+config.account = 'IAR75129927';
 
 var order = {
 	account:config.account,
@@ -16,14 +17,48 @@ var order = {
 	orderType:"limit"
 };
 
+var priceAverage = [];
+
+function getPrice(venue, ticker) {
+	var q = $q.defer();
+	agent
+		.get('http://api.stockfighter.io/ob/api/venues/'+venue+'/stocks/'+ticker+'/quote')
+		.set('X-Starfighter-Authorization', config.key)
+		.end(function(err,res) {
+			if (err) return q.reject(err);
+			q.resolve(res.body);
+		});
+	return q.promise;
+};
+
+function recursivePrice() {
+	getPrice(config.venue, config.ticker).then(function(price) {
+		priceAverage.push(price.askSize);
+		if (priceAverage.length === 5) {
+			console.log('');
+			console.log('Average prices for',config.ticker);
+			console.log(priceAverage);
+		} else {
+			setTimeout(recursivePrice, 5000);
+		}
+	}, function(err) {
+		throw err;
+	});
+}
+
+
+
+
+
+
+
+
+
+
+
 var myPrice = 5800;
 
-agent
-	.get('http://api.stockfighter.io/ob/api/venues/'+config.venue+'/stocks/'+config.ticker+'/quote')
-	.set('X-Starfighter-Authorization', config.key)
-	.end(function(err,res) {
-		console.log(err||res.body);
-	});
+
 
 
 // agent
